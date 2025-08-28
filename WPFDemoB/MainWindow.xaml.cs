e1f6b9c257c1546b845bdbbd49251df0b7e183a1
AndyMB94 <andy.mallcco@tecsup.edu.pe>
2025-08-28 18:37:38 -0500

    mallcco

--- WPFDemoB/MainWindow.xaml.cs
+++ WPFDemoB/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ namespace WPFDemoB
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// asdf
     public partial class MainWindow : Window
     {
         //Mi primer comentario Mallcco
